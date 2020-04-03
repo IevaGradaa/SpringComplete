3e6cee0d8ae15724442ee41a424b181decad76fd
ievagradaa <ieva.grada@gmail.com>
2020-04-03 09:50:05 +0300

    Add annotations

--- src/main/java/hello/repository/CustomerRepository.java
+++ src/main/java/hello/repository/CustomerRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     List<Customer> findAll();
 
-    //TODO: Add code that will be necessary to implement all methods in service
+    //TODO: Add code that will be necessary to implement all methods in servic
     public List<Customer> findByLastName(String lastName);
     public Customer findCustomerById(Long id);
     public List<Customer> findCustomerBy(String searchKey);
