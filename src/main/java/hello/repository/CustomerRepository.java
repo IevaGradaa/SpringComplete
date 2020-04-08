f35a877e514eb3ea50f6cf369f467b3831d70e80
ievagradaa <ieva.grada@gmail.com>
2020-04-08 08:36:55 +0300

    Change repository method names according to naming convention

--- src/main/java/hello/repository/CustomerRepository.java
+++ src/main/java/hello/repository/CustomerRepository.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import hello.model.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     List<Customer> findAll();
 
     //TODO: Add code that will be necessary to implement all methods in service
-    public List<Customer> findByLastName(String lastName);
-    public Customer findCustomerById(Long id);
-    public List<Customer> findCustomerBy(String searchKey);
-    public void update(Customer customer);
+    List<Customer> findByLastName(String lastName);
+    Customer findCustomerById(Long id);
+    List<Customer> findBySearchKey(String searchKey);
+    Customer save(Customer customer);
+
 }
